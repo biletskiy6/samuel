e9abbba3a8f50a0a4535f06249ae28ea47ae2be5
biletskiy6 <victorbiletskiy82@gmail.com>
2020-10-28 18:40:54 +0200

    header updates

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -1,12 +1,20 @@
 $(function(){
 	svg4everybody();
-	//
-	// var rellax = new Rellax('.rellax', {
-	// 	speed: -2,
-	// 	center: false,
-	// 	wrapper: null,
-	// 	round: true,
-	// 	vertical: true,
-	// 	horizontal: false
-	// });
+
+	var rellax = new Rellax('.rellax', {
+		speed: -2,
+		center: false,
+		wrapper: null,
+		round: true,
+		vertical: true,
+		horizontal: false
+	});
+});
+let lastScrollTop = 0;
+$(window).scroll(function () {
+	let st = $(this).scrollTop();
+	let $topLine = $('.top-line');
+	st > 0 ? $topLine.addClass('hidden') : $topLine.removeClass('hidden');
+	st > 150 ? $topLine.removeClass('hidden') : null;
+
 });
