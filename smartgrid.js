108fe949125c72cb843c17cb76527adf9c131f6c
Victor Biletskiy <victorbiletskiy82@gmail.com>
2020-10-30 09:23:25 +0200

    animations setup

--- smartgrid.js
+++ smartgrid.js
@@ -1,53 +1,40 @@
 var smartgrid = require('smart-grid');
 
 var settings = {
-    outputStyle: 'sass',
-    columns: 12,
-    offset: '30px',
-    mobileFirst: false,
-    container: {
-        maxWidth: '1410px',
-        fields: '20px'
+  outputStyle: 'sass',
+  columns: 12,
+  offset: '30px',
+  mobileFirst: false,
+  container: {
+    maxWidth: '1410px',
+    fields: '20px',
+  },
+  breakPoints: {
+    md: {
+      width: '1366px',
+      fields: '15px',
     },
-    breakPoints: {
-        lg: {
-            width: '1920px',
-            fields: '20px'
-        },
-        desktop: {
-            width: '1440px',
-            fields: '15px'
-        },
-        md: {
-            width: '1366px',
-            fields: '15px'
-
-        },
-        mdx: {
-            width: '1280px',
-            fields: '15px'
-
-        },
-        tablet: {
-            width: '1024px',
-            fields: '15px'
-
-        },
-        tabletx: {
-            width: '768px',
-            fields: '15px'
-
-        },
-        phone: {
-            width: '480px',
-            fields: '10px'
-        },
-        iphone: {
-            width: '320px',
-            fields: '10px'
-        }
-
-    }
+    mdx: {
+      width: '1280px',
+      fields: '15px',
+    },
+    tablet: {
+      width: '992px',
+      fields: '15px',
+    },
+    tabletx: {
+      width: '768px',
+      fields: '15px',
+    },
+    phone: {
+      width: '480px',
+      fields: '10px',
+    },
+    iphone: {
+      width: '320px',
+      fields: '10px',
+    },
+  },
 };
 
 smartgrid('./src/static/sass', settings);
--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -1,20 +1,113 @@
-$(function(){
-	svg4everybody();
+function animatePreloader() {
+  var tl = gsap.timeline();
+  tl.fromTo(
+    '#overlay-1 h1',
+    { y: 0, autoAlpha: 1 },
+    { y: -40, autoAlpha: 0, delay: 0.5 },
+  )
+    .fromTo(
+      '#overlay-1',
+      { left: 0, ease: Power2.easeInOut },
+      { left: '110%', ease: Expo.easeInOut, duration: 2 },
+    )
+    .fromTo(
+      '#overlay-2',
+      { left: 0, ease: Power2.easeInOut },
+      { left: '110%', ease: Expo.easeInOut, duration: 2, delay: -1.9 },
+    );
+}
 
-	var rellax = new Rellax('.rellax', {
-		speed: -2,
-		center: false,
-		wrapper: null,
-		round: true,
-		vertical: true,
-		horizontal: false
-	});
-});
-let lastScrollTop = 0;
-$(window).scroll(function () {
-	let st = $(this).scrollTop();
-	let $topLine = $('.top-line');
-	st > 0 ? $topLine.addClass('hidden') : $topLine.removeClass('hidden');
-	st > 150 ? $topLine.removeClass('hidden') : null;
+function animateMenuIn(tl, done) {
+  $('.mobile-menu').addClass('opened');
+  tl.to('.menuRow', {
+    top: 0,
+    left: 0,
+    ease: Expo.easeInOut,
+    duration: 1,
+  }).to('.menuItem', {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    ease: 'back.easeOut.config(1.2)',
+    stagger: {
+      each: 0.09,
+    },
+    onComplete: done,
+  });
+}
 
+function animateMenuOut(tl, done) {
+  $('.mobile-menu').removeClass('opened');
+  tl.to('.menuRow', {
+    top: '9999px',
+    ease: Expo.easeInOut,
+    duration: 2,
+  }).to('.menuItem', {
+    y: 20,
+    opacity: 0,
+    scale: 0,
+    ease: 'back.easeOut.config(1.2)',
+    delay: -2,
+    stagger: {
+      each: 0.1,
+    },
+    onComplete: done,
+  });
+}
+
+function animateCursors() {
+  var $cursor = $('.cursor');
+  var $cursor2 = $('.cursor2');
+
+  $(document).on('mousemove', function (e) {
+    $cursor.css({
+      left: e.clientX,
+      top: e.clientY,
+    });
+    $cursor2.css({
+      left: e.clientX,
+      top: e.clientY,
+    });
+  });
+}
+
+$(function () {
+  svg4everybody();
+
+  // animatePreloader();
+  var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+  });
+
+  let isAnimationCompleted = false;
+  let isTriggered = false;
+  $('.mobile-menu').on('click', function () {
+    var tl = gsap.timeline();
+    if (!isAnimationCompleted) {
+      $(this).attr('disabled', true);
+      animateMenuIn(tl, function () {
+        isAnimationCompleted = true;
+        $('.mobile-menu').attr('disabled', false);
+      });
+    } else {
+      $(this).attr('disabled', true);
+      animateMenuOut(tl, function () {
+        isAnimationCompleted = false;
+        $('.mobile-menu').attr('disabled', false);
+      });
+    }
+  });
 });
+// let lastScrollTop = 0;
+// $(window).scroll(function () {
+// 	let st = $(this).scrollTop();
+// 	let $topLine = $('.top-line');
+// 	st > 0 ? $topLine.addClass('hidden') : $topLine.removeClass('hidden');
+// 	st > 150 ? $topLine.removeClass('hidden') : null;
+
+// });
